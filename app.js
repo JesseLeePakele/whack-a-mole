45a9b73691f04ff4cbee8ccbb935dbbc6236ae40
Jesse-Lee Pakele <jesse-leepakele@Jesse-Lees-MacBook-Air.local>
2019-04-23 10:39:05 -1000

    finished project

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
-window.onload = function(){
-
+window.onload = function()
+{
+    var dirt = document.getElementsByClassName('dirt');
+    var sound = document.getElementById("sound"); 
+    var grid = document.getElementById("dirt-box"); 
     
-var dirt = document.getElementByClass("dirt");
+    var score = 0;    
+    
+    for (let i = 0; i < dirt.length; i++)
+    {
+        dirt[i].addEventListener("click", function click() 
+        {
+            if (dirt[i].innerHTML)
+            {
+                sound.play();
+                dirt[i].innerHTML = "";
+                score++;
+                document.getElementById("score").innerHTML = "Score: " + score;
 
-var sound = document.getElementById("sound");
-
-var grid = document.getElementById("dirtbox");
-
-for(i=0; i<dirt.length i++){
-    dirt[i].addEventListener("click", function(){
-        if(dirt.innerHTML == ""){
-            console.log("nothing is here");
-        }else{
-        dirt.innerHTML == "";
-        sound.onplay();
+            }
+            else
+            {
+                console.log("Nothing is there.");
             }
         });
-    }
+    };
 
-    function setMole(){
-        var mole = document.createElement("DIV");
-    }
+
+setInterval (function setMole()
+{
+    var mole = document.createElement("div");
+    mole.setAttribute("class", "mole");
+    var random = Math.floor(Math.random() * Math.floor(dirt.length));
     
-
-}
+    if (dirt[random].innerHTML === "")
+    {
+        dirt[random].appendChild(mole);
+    }
+    else
+    {
+        console.log("There are too many moles!");
+    }
+}, 1000);
+};
